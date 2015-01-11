f1db40dc2d53e530c66a34b0ba408956e06a2436
Dinh-Hung-Tu <dinhhungtu@gmail.com>
2015-01-11 22:05:00 +0800

    disable input directly to the screen render multiplication with "x" rather than "*"

--- calculator.js
+++ calculator.js
@@ -2,41 +2,53 @@
 //More can be added. 
 //Reference: http://www.codeproject.com/Tips/631525/Creating-Calculator-using-HTML-CSS-and-JavaScript
 //Goal: Follow the simplistic calculator in the Window. Don't result in error. Handle the repetition. 
-var clear = false;
+//to fix: fix the display of text, try to get the text scroll. If possible make two lines Make the screen is immutable.
+var clear = true;
 var mr = 0;
 var operator = ["+","-","*","/"];
+	
   function c(val) //Clear and replace by val
   {
-      document.getElementById("screen").value=val;
+      document.getElementById("screen").value=val;	  
   }
   function append(val) //Append val to the end
   {
       if (clear == true) {
 	  c(val); //From clear state -> register first value	  
 	  clear = false;
-	  }
-	  else 
+	  }	  
+	  else
 	  {
-		 var current_display = document.getElementById("screen").value;
-		 var last_press = current_display[current_display.length - 1];
+		var current_display = document.getElementById("screen").value;
+		var char_set = current_display.split('');
+		var last_press = current_display[current_display.length - 1];
+		
 		if(operator.indexOf(val)>=0 && operator.indexOf(last_press)>=0) //Change the sign if the last pressed button is sign
 			{
-				//current_display[current_display.length - 1] = val; Error: String in Javascript is immutable
-				var char_set = current_display.split('');
+				//current_display[current_display.length - 1] = val; Error: String in Javascript is immutable				
+				
 				char_set[current_display.length - 1] = val;
 				document.getElementById("screen").value = char_set.join("");
-			}
-		else document.getElementById("screen").value += val;
-		clear = false;	//has some content
-		}
+			}				
+		else document.getElementById("screen").value += val;		
+		clear = false;	//has some content		
+		
+  }
   }
   function get_result() 
   { 
 	if (clear == false)
 	{
-      try 
+      try 	  
       { 
-        c(eval(document.getElementById("screen").value));
+		var current_display = document.getElementById("screen").value;
+		var char_set = current_display.split('');
+		while (char_set.indexOf("x")>=0)
+		{
+			char_set[char_set.indexOf("x")] = "*";
+		}
+		result = eval(char_set.join(""));
+        c(result.toString());
 		clear = true;
       } 
       catch(e) 
@@ -77,6 +89,7 @@ var operator = ["+","-","*","/"];
   {
 	var current_display = document.getElementById("screen").value;
 	var char_set = current_display.split('');
+
 	if (char_set[0] == "-") 
 	{
 		char_set[0] = ""		  
@@ -95,4 +108,39 @@ var operator = ["+","-","*","/"];
   {
 		mr = 0;
   }
-  
+  document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+	var keycode = e.keyCode;
+	console.log(keycode);
+    if (keycode == '38') {
+        // up arrow
+		console.log("Up");
+    }
+    else if (keycode >= 48 && keycode <= 57) //Letters A to Z and standard numbers 0 to 9
+	{
+		var digit = keycode - 48;		
+		append(digit.toString());
+	}		
+	else if (keycode >= 96 && keycode <= 105) //Letters A to Z and standard numbers 0 to 9
+	{
+		var digit = keycode - 96;
+		append(digit.toString());
+	}	
+	else if (keycode == 106) append("*"); //
+	else if (keycode == 107 || keycode == 187) append("+"); //
+	else if (keycode == 109 || keycode == 189) append("-"); //
+	else if (keycode == 110 || keycode == 190) append("."); //decimal 
+	else if (keycode == 111 || keycode == 191) append("/"); //
+	else if (keycode == 13) get_result(); //Enter
+	else if (keycode == 8) clear_last(); //Backspace
+}
+function clear_last()
+{
+	var current_display = document.getElementById("screen").value;
+	var char_set = current_display.split('');
+	char_set.pop();
+	document.getElementById("screen").value = char_set.join("");
+}
